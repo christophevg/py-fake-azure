d1c9c516f7df39eae3b7eac619be2f5f5d0e7c6c
Christophe VG <contact@christophe.vg>
2022-10-23 11:32:04 +0200

    added stubbed support for monitor querying

---
+++ azure/identity.py
@@ -0,0 +1,2 @@
+class DefaultAzureCredential():
+  pass
---
+++ azure/monitor/query.py
@@ -0,0 +1,22 @@
+class LogsQueryClient() :
+  def __init__(self, credential):
+    pass
+
+  def query_workspace(self, workspace_id, query, timespan=None):
+    return
+
+class MetricsQueryClient():
+  def __init__(self, credential):
+    pass
+
+class LogsQueryStatus():
+  PARTIAL = 1
+  SUCCESS = 2
+
+class LogsQueryResponse():
+  def __init__(self):
+    self.status = LogsQueryStatus.SUCCESS
+    self.tables = []
+    
+    self.partial_error = None
+    self.partial_data = None
